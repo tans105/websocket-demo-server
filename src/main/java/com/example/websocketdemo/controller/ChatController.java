ee63e78c0b11cb661a2a23fc3c4b789fdba87972
Tanmay Awasthi <tanmayawasthi105@gmail.com>
2020-05-17 01:30:42 +0530

    Sending custom messages object

--- src/main/java/com/example/websocketdemo/controller/ChatController.java
+++ src/main/java/com/example/websocketdemo/controller/ChatController.java
@@ -6,7 +6,6 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.util.HtmlUtils;
 
 /**
  * @author Tanmay
@@ -18,8 +17,8 @@ public class ChatController {
 
     @MessageMapping("/chat.sendMessage")
     @SendTo("/topic/public")
-    public String sendMessage(String chatMessage) {
-        return "Hi " + chatMessage;
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        return chatMessage;
     }
 
     @MessageMapping("/chat.addUser")
--- src/main/java/com/example/websocketdemo/model/ChatMessage.java
+++ src/main/java/com/example/websocketdemo/model/ChatMessage.java
@@ -1,13 +1,36 @@
 package com.example.websocketdemo.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Date;
+
 /**
  * @author Tanmay
  * @date 15/05/20
  **/
 public class ChatMessage {
+
+    @JsonProperty("_type")
     private MessageType type;
+
+    @JsonProperty("_content")
     private String content;
+
+    @JsonProperty("_sender")
     private String sender;
+
+    @JsonProperty("_timestamp")
+    private Date timestamp;
+
+    @Override
+    public String toString() {
+        return "ChatMessage{" +
+                "type=" + type +
+                ", content='" + content + '\'' +
+                ", sender='" + sender + '\'' +
+                ", timestamp=" + timestamp +
+                '}';
+    }
 
     public enum MessageType {
         CHAT,
@@ -37,5 +60,13 @@ public class ChatMessage {
 
     public void setSender(String sender) {
         this.sender = sender;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
     }
 }
